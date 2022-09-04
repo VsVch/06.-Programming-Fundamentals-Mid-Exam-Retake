c4242629047b447ae91467a1b075228a14e1d8a4
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-09-04 20:22:09 +0300

    Add project files.

---
+++ 01. Black Flag/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._Black_Flag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int dailyPlumber = int.Parse(Console.ReadLine());
+            double expectedPlumber = double.Parse(Console.ReadLine());
+
+            double totalPlumber = 0;
+            for (int i = 1; i <= days; i++)
+            {
+                totalPlumber += dailyPlumber;
+
+                if (i % 3 == 0)
+                {
+                    totalPlumber += dailyPlumber * 0.5;
+                }
+                if (i % 5 == 0)
+                {
+                    totalPlumber -= totalPlumber * 0.3;
+                }
+
+            }
+            if (totalPlumber >= expectedPlumber)
+            {
+                Console.WriteLine($"Ahoy! {totalPlumber:f2} plunder gained.");
+            }
+            else
+            {
+                Console.WriteLine($"Collected only {(totalPlumber / expectedPlumber * 100):f2}% of the plunder.");
+            }
+        }
+    }
+}
---
+++ 06. Programming Fundamentals Mid Exam Retake/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Programming_Fundamentals_Mid_Exam_Retake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
